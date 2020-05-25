294ec08af0c4dce32a826149640a110211208dd3
Michele T <aedonem@gmail.com>
2020-05-25 09:49:26 +0200

    Updates

---
+++ precache-manifest.0b99558f51e326608f9256d5af997c06.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "076f7621778e9b42a4d2844aed4f1801",
+    "url": "/calendar-scheduler/index.html"
+  },
+  {
+    "revision": "074139ea3423b82b07e9",
+    "url": "/calendar-scheduler/static/css/main.e029b5a8.chunk.css"
+  },
+  {
+    "revision": "91c032a2e4a7710560a8",
+    "url": "/calendar-scheduler/static/js/2.06f4e7d6.chunk.js"
+  },
+  {
+    "revision": "074139ea3423b82b07e9",
+    "url": "/calendar-scheduler/static/js/main.934225b7.chunk.js"
+  },
+  {
+    "revision": "a4169b5a540685bd79f0",
+    "url": "/calendar-scheduler/static/js/runtime-main.6a4c197d.js"
+  }
+]);
